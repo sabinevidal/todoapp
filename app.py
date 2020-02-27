0490aabcd17d4763c86c3a232c4cd49098bd3872
Sabine Vidal <svidal@live.co.za>
2020-02-27 11:57:45 +0200

    Adding CRUD lists

--- app.py
+++ app.py
@@ -14,9 +14,17 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -66,7 +74,11 @@ def delete_todo(todo_id):
         db.session.close()
     return jsonify({'success': True })
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html', data=Todo.query.filter_by(list_id=list_id).order_by('id').all()
+)
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all()
-  )
+    return redirect(url_for('get_list_todos', list_id=1))
